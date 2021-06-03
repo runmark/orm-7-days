7f987232157ecc39a84d039238c829e0a82c6abb
Ryan Mark <ma_xingmin@163.com>
2021-06-04 06:02:39 +0800

    feat(schema): use schema to convert between go struct and db table

--- schema/schema.go
+++ schema/schema.go
@@ -1,5 +1,11 @@
 package schema
 
+import (
+	"example.com/mark/geeorm/dialect"
+	"go/ast"
+	"reflect"
+)
+
 type Field struct {
 	Name string
 	Type string
@@ -16,4 +22,43 @@ type Schema struct {
 
 func (s *Schema) GetField(name string) *Field {
 	return s.FieldMap[name]
+}
+
+func Parse(origin interface{}, dialect dialect.Dialect) (schema *Schema) {
+	rv := reflect.Indirect(reflect.ValueOf(origin))
+	rt := rv.Type()
+
+	schema = &Schema{
+		Model:    origin,
+		Name:     rt.Name(),
+		FieldMap: make(map[string]*Field),
+	}
+
+	for i := 0; i < rt.NumField(); i++ {
+
+		rfield := rt.Field(i)
+
+		if !rfield.Anonymous && ast.IsExported(rfield.Name) {
+
+			schema.FieldNames = append(schema.FieldNames, rfield.Name)
+
+			field := Field{}
+			field.Name = rfield.Name
+			//field.Type = dialect.DataTypeOf(reflect.New(rfield.Type).Elem())
+			field.Type = dialect.DataTypeOf(reflect.Indirect(reflect.New(rfield.Type)))
+
+			rtag := rfield.Tag
+			t, ok := rtag.Lookup("geeorm")
+			if ok {
+				field.Tag = t
+			}
+
+			schema.Fields = append(schema.Fields, &field)
+
+			schema.FieldMap[field.Name] = &field
+		}
+
+	}
+
+	return
 }
---
+++ schema/schema_test.go
@@ -0,0 +1,54 @@
+package schema
+
+import (
+	"example.com/mark/geeorm/dialect"
+	"reflect"
+	"testing"
+)
+
+type Person struct {
+	Id   int `geeorm:"PRIMARY KEY"`
+	Name string
+}
+
+func TestParse(t *testing.T) {
+	cases := []struct {
+		input       interface{}
+
+		oFieldNames []string
+		oFieldTypes []string
+		oFieldTags  []string
+	}{
+		{
+			Person{3, "John"},
+
+			[]string{"Id", "Name"},
+			[]string{"integer", "text"},
+			[]string{"PRIMARY KEY", ""},
+		},
+	}
+
+	d, _ := dialect.GetDialect("sqlite3")
+
+	for _, c := range cases {
+
+		s := Parse(c.input, d)
+
+		if !reflect.DeepEqual(s.FieldNames, c.oFieldNames) {
+			t.Errorf("expect %s, but got %s\n", c.oFieldNames, s.FieldNames)
+		}
+
+		sFieldTypes, sFieldTags := []string{}, []string{}
+
+		for _, f := range s.Fields {
+			sFieldTypes = append(sFieldTypes, f.Type)
+			sFieldTags = append(sFieldTags, f.Tag)
+		}
+
+		if !reflect.DeepEqual(sFieldTypes, c.oFieldTypes) || !reflect.DeepEqual(sFieldTags, c.oFieldTags) {
+			t.Errorf("expect (%s, %s), but got (%s, %s)\n", c.oFieldTypes, c.oFieldTags, sFieldTypes, sFieldTags)
+		}
+
+	}
+
+}

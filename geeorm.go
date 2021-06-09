c9171db2d59b52d5bd0da93dc8ab821dc9d5d243
Ryan Mark <ma_xingmin@163.com>
2021-06-09 18:39:10 +0800

    feat(geeorm): 增加数据库migrate表功能

--- geeorm.go
+++ geeorm.go
@@ -5,14 +5,16 @@ import (
 	"example.com/mark/geeorm/dialect"
 	"example.com/mark/geeorm/log"
 	"example.com/mark/geeorm/session"
+	"fmt"
+	"strings"
 )
 
 type Engine struct {
-	db *sql.DB
+	db      *sql.DB
 	dialect dialect.Dialect
 }
 
-func NewEngine(driver, source string) (e *Engine, err error){
+func NewEngine(driver, source string) (e *Engine, err error) {
 	db, err := sql.Open(driver, source)
 	if err != nil {
 		log.Error(err)
@@ -31,7 +33,7 @@ func NewEngine(driver, source string) (e *Engine, err error){
 		return
 	}
 
-	e = &Engine{db,dialect}
+	e = &Engine{db, dialect}
 
 	log.Info("Connect db success!")
 
@@ -66,19 +68,83 @@ func (e *Engine) Transaction(txFunc TxFunc) (result interface{}, err error) {
 
 		if p := recover(); p != nil {
 			_ = s.Rollback()
-			panic(p)  // re-throw panic after Rollback
-		} else if err != nil  {
-			_ = s.Rollback()  // err is non-nil; don't change it
+			panic(p) // re-throw panic after Rollback
+		} else if err != nil {
+			_ = s.Rollback() // err is non-nil; don't change it
 		} else {
 			defer func() {
 				if err != nil {
 					_ = s.Rollback()
 				}
 			}()
-			err = s.Commit()  // err is nil; if Commit returns error update err
+			err = s.Commit() // err is nil; if Commit returns error update err
 		}
 
 	}()
 
 	return txFunc(s)
+}
+
+// a - b
+func difference(as []string, bs []string) (diff []string) {
+
+	bm := map[string]struct{}{}
+
+	for _, b := range bs {
+		bm[b] = struct{}{}
+	}
+
+	for _, a := range as {
+		if _, ok := bm[a]; !ok {
+			diff = append(diff, a)
+		}
+	}
+
+	return
+}
+
+func (e *Engine) Migrate(value interface{}) (err error) {
+
+	_, err = e.Transaction(func(s *session.Session) (result interface{}, err error) {
+
+		if !s.Model(value).HasTable() {
+			log.Infof("table %s doesnt exists!", s.RefTable().Name)
+			return nil, s.CreateTable()
+		}
+
+		rows, _ := s.Raw(fmt.Sprintf("SELECT * FROM %s LIMIT 1;", s.RefTable().Name)).QueryRows()
+		oldColumns, _ := rows.Columns()
+
+		addedFields := difference(s.RefTable().FieldNames, oldColumns)
+		deletedFields := difference(oldColumns, s.RefTable().FieldNames)
+
+		log.Infof("added cols %v, deleted cols %v", addedFields, deletedFields)
+
+		for _, field := range addedFields {
+			f := s.RefTable().GetField(field)
+			_, err = s.Raw(fmt.Sprintf("ALTER TABLE %s ADD COLUMN %s %s;", s.RefTable().Name, field, f.Type)).Exec()
+			if err != nil {
+				return
+			}
+		}
+
+		if len(deletedFields) == 0 {
+			return
+		}
+
+		table := s.RefTable()
+		tmpTableName := table.Name + "_tmp"
+
+		fieldsStr := strings.Join(table.FieldNames, ",")
+
+		s.Raw(fmt.Sprintf("CREATE TABLE %s AS SELECT %s FROM %s;", tmpTableName, fieldsStr, table.Name))
+		s.Raw(fmt.Sprintf("DROP TABLE %s;", table.Name))
+		s.Raw(fmt.Sprintf("ALTER TABLE %s RENAME TO %s;", tmpTableName, table.Name))
+
+		_, err = s.Exec()
+
+		return
+	})
+
+	return
 }
